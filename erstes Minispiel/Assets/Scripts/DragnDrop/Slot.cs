3be778536474977af7fe1e4d2e75ba328589d42d
Ahmad Moudallal <ahmad.moudallal1@hotmail.com>
2018-07-08 17:55:50 +0200

    drag and drop prototype, hover events begin

---
+++ erstes Minispiel/Assets/Scripts/DragnDrop/Slot.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class Slot : MonoBehaviour, IDropHandler {
+    public GameObject item1
+    {
+        get
+        {
+            if(transform.childCount>0)
+            {
+                return transform.GetChild (0).gameObject;
+            }
+            return null;
+        }
+    }
+    public GameObject item2
+    {
+        get
+        {
+            if (transform.childCount > 1)
+            {
+                return transform.GetChild(1).gameObject;
+            }
+            return null;
+        }
+    }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        DragHandler.itemBeingDragged.transform.SetParent(transform);
+        if(transform.childCount == 2)
+        {
+            GameObject.Find("grayNew").GetComponent<Image>().enabled = true;
+            GameObject.Find("Draggables").SetActive(false);
+            GameObject.Find("DropPlaces").SetActive(false);
+        }
+    }
+}
---
+++ erstes Minispiel/Assets/Scripts/Hover.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Hover : MonoBehaviour {
+    public string clickedOjbectName;
+
+    void OnMouseEnter()
+    {
+        print("Mouse is over GameObject.");
+        Debug.Log("Mouse is over GameObject.");
+    }
+
+    void OnMouseExit()
+    {
+    }
+}
