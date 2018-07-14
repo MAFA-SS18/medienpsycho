c4f5d64f3cd48c5a4be2bfb898e00d54b1948dd1
Adrian Jentzsch <jentzsch.adrian@web.de>
2018-07-14 13:18:50 +0200

    1. Minispiel

--- erstesMinispiel/Assets/Scripts/minispiel/blueController.cs
+++ erstesMinispiel/Assets/Scripts/minispiel/blueController.cs
@@ -8,10 +8,14 @@ public class blueController : MonoBehaviour {
     public Rigidbody2D rb;
     public float delay = 0.5f;
     public GameManager gamemanager;
+    public GameObject Score;
+    public score scoreskript;
 
     private void Start()
     {
         gamemanager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        Score = GameObject.Find("Score");
+        scoreskript = (score)Score.GetComponent(typeof(score));
     }
 
     // Update is called once per frame
@@ -27,9 +31,13 @@ public class blueController : MonoBehaviour {
         switch (gamemanager.stage)
         {
             case 9:
+                Destroy(gameObject);
+                scoreskript.scoreUpdate();
                 gamemanager.CompleteLevel();
                 break;
             default:
+                Destroy(gameObject);
+                scoreskript.resetScore();
                 gamemanager.EndGame();
                 break;
         }
---
+++ erstesMinispiel/Assets/score.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class score : MonoBehaviour {
+
+    public int scorepoints = 0;
+    public Text scoretext;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void scoreUpdate()
+    {
+        scorepoints += 1;
+        scoretext.text = scorepoints.ToString();
+    }
+
+    public void resetScore()
+    {
+        scorepoints = 0;
+        scoretext.text = scorepoints.ToString();
+    }
+}
